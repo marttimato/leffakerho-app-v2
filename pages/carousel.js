f617a578effe76ed201488f7bf19d4aa010c8483
Mikko <mikko@leffakerho.app>
2026-02-20 08:03:52 +0200

    Remove 'Katso tiedot' text from carousel movie cards

--- pages/carousel.js
+++ pages/carousel.js
@@ -145,12 +145,7 @@ export default function Carousel() {
                                             )}
                                         </div>
 
-                                        <div className="flex items-center gap-2 text-blue-500 font-bold text-[10px] uppercase tracking-widest">
-                                            <span>Katso tiedot</span>
-                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="currentColor" className="w-3 h-3">
-                                                <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-                                            </svg>
-                                        </div>
+                                        {/* Removed "Katso tiedot" text to keep UI cleaner as requested */}
                                     </div>
                                 </div>
                             ))}
